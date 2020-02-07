e8ce2c10c8e5d6f0d39e7b31cf6da07e57ba25b6
Mariano Mauricio <marianomauricio72@gmail.com>
2020-02-06 18:58:05 -0600

    pendiente

--- main.cpp
+++ main.cpp
@@ -8,10 +8,16 @@ public:
     Figura(int _x)
     {
         x = _x;
+        cout << "hola soy una figura "<< endl;
     }  
 
     virtual float perimetro();
     virtual float area();              
+};
+
+class circulo: public Figura {
+    public:
+        circulo(int r) : Figura(r) {}
 };
 
 int main()
